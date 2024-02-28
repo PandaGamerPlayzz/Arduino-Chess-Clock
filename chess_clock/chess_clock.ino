31fb1b59c406f0f43abf31b280d1ff5caff612bb
PandaGamerPlayzz <zacharymiller515@gmail.com>
2024-02-27 20:21:01 -0500

    E

--- chess_clock/chess_clock.ino
+++ chess_clock/chess_clock.ino
@@ -2,20 +2,28 @@
 #include <TM1637Display.h>
 
 // Module connection pins (Digital Pins)
-#define CLK  2
-#define CLK2 4
-#define DIO  3
-#define DIO2 5
+const int CLK = 2;
+const int CLK2 = 4;
+const int DIO = 3;
+const int DIO2 = 5;
+
+const int BTN_P1 = A0;
+const int BTN_P2 = A1;
+const int BTN_RESET = A2;
 
 // The amount of time (in milliseconds) between tests
-#define DELAY 1000
+const int DELAY = 1000;
 
-TM1637Display display(CLK, DIO);
+TM1637Display display1(CLK, DIO);
 TM1637Display display2(CLK2, DIO2);
 
 double timer1 = 500.0;
 double timer2 = 500.0;
 int player = 2;
+
+int btnP1State = 0;
+int btnP2State = 0;
+int btnResetState = 0;
 
 unsigned long lastTime = micros();
 
@@ -27,27 +35,41 @@ int formatTime(int seconds) {
 
 void setup() {
   Serial.begin(9600);
+
+  pinMode(BTN_P1, INPUT);
+  pinMode(BTN_P2, INPUT);
+  pinMode(BTN_RESET, INPUT);
 }
 
 void loop() {
   unsigned long currentTime = micros();
   unsigned long deltaTime = (currentTime - lastTime);
 
-  Serial.println(timer2);
+  btnP1State = digitalRead(BTN_P1);
+  btnP2State = digitalRead(BTN_P2);
+  btnResetState = digitalRead(BTN_RESET);
+
+  // Serial.print(btnP1State);
+  // Serial.print(", ");
+
+  // Serial.print(btnP2State);
+  // Serial.print(", ");
+
+  // Serial.println(btnResetState);
 
   if(player == 1) {
     timer1 -= deltaTime * 0.000001;
 
-    display.setBrightness(0xff);
+    display1.setBrightness(0xff);
     display2.setBrightness(0x00);
   } else if(player == 2) {
     timer2 -= deltaTime * 0.000001;
 
-    display.setBrightness(0x00);
+    display1.setBrightness(0x00);
     display2.setBrightness(0xff);
   }
-
-  display.showNumberDecEx(formatTime((int)timer1), (0x80 >> 1), false);
+  
+  display1.showNumberDecEx(formatTime((int)timer1), (0x80 >> 1), false);
   display2.showNumberDecEx(formatTime((int)timer2), (0x80 >> 1), false);
 
   lastTime = currentTime;
